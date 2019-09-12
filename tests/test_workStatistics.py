83a2b4923f2833ac18cc118e90c13eadc3d7068c
andrey <a.i.maslov199907@bk.ru>
2019-09-12 12:30:44 +0300

    доработка тестов

--- tests/test_workStatistics.py
+++ tests/test_workStatistics.py
@@ -1,9 +1,43 @@
-from pytest import fail, raises
+import sqlite3 as sqlite
+from pytest import raises, skip
+from freezegun import freeze_time
 from wtc.work_statistics_monitor import WorkStatistics
+from datetime import datetime, date, timedelta
+from random import randint
+from typing import *
 
 DATABASE = 'testdb.sqlite'
 
 
+def sql_period_length_call(
+        conn: sqlite.Connection,
+        begin: Union[int, float, date, datetime],
+        end: Union[int, float, date, datetime]):
+
+    if type(begin) is not int:
+        if type(begin) is float:
+            begin = int(begin)
+        elif type(begin) is datetime or type(begin) is date:
+            begin = int(begin.timestamp())
+        else:
+            raise ValueError
+
+    if type(end) is not int:
+        if type(end) is float:
+            end = int(end)
+        elif type(end) is datetime or type(end) is date:
+            end = int(end.timestamp())
+        else:
+            raise ValueError
+
+    return conn.execute(
+                f"select sum(min({end}, last_timestamp) - max({begin}, first_timestamp)) "
+                f"from active_time "
+                f"where last_timestamp > {begin} and first_timestamp < {end}"
+            ).fetchone()[0] or 0
+
+
+@freeze_time(lambda: datetime.fromtimestamp(1567633936))
 class TestWorkStatistics:
     def test_init(self):
         ws = WorkStatistics()
@@ -30,10 +64,39 @@ class TestWorkStatistics:
         assert ws.year >= ws.month >= ws.week >= ws.day
 
     def test_period_stat(self):
-        fail()  # TODO
+        def step_generator(begin: datetime) -> Iterable[datetime]:
+            t = begin
+            t += timedelta(seconds=randint(2, 15))
+            while t <= datetime.now():
+                yield t
+                t += timedelta(seconds=randint(50, 500))
+
+        ws = WorkStatistics.from_db(DATABASE)
+        conn = sqlite.connect(DATABASE)
+        begin = datetime.now() - timedelta(days=365)
+        for it in step_generator(begin):
+            start = datetime.fromtimestamp(randint(begin.timestamp(), it.timestamp() - 1))
+            ps = ws.period_stat(start, it)
+            assert ps.total_seconds() == sql_period_length_call(conn, start, it)
+
+        # TODO: то же, но без кэша
 
     def test_update(self):
-        fail()  # TODO
+        skip('TODO')
 
     def test_ymwd(self):
-        fail()  # TODO
+        now = datetime.now()
+        day_begin = datetime.fromordinal(now.toordinal())
+        week_begin = datetime(now.year, now.month, now.day) - timedelta(days=now.weekday())
+        month_begin = datetime(now.year, now.month, 1)
+        year_begin = datetime(now.year, 1, 1)
+
+        conn = sqlite.connect(DATABASE)
+        ws = WorkStatistics.from_db(DATABASE)
+
+        assert ws.day.total_seconds() == sql_period_length_call(conn, day_begin, now)
+        assert ws.week.total_seconds() == sql_period_length_call(conn, week_begin, now)
+        assert ws.month.total_seconds() == sql_period_length_call(conn, month_begin, now)
+        assert ws.year.total_seconds() == sql_period_length_call(conn, year_begin, now)
+
+        # TODO: то же, но без кэша
