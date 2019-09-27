fbe299af80925343ed55f413bd2de42ba4b100be
andrey <a.i.maslov199907@bk.ru>
2019-09-27 23:45:27 +0300

    избавление от излишнего метода класса DatabaseManager и удаление лишнего импорта

--- tests/db_menenger.py
+++ tests/db_menenger.py
@@ -1,4 +1,3 @@
-from sqlalchemy import func as sqlFunc
 from contextlib import contextmanager
 from typing import *
 
@@ -21,17 +20,10 @@ class DatabaseManager:
         session.rollback()
 
     def get_work_time_in_period(self, p: Period) -> int:
-        total_time = 0
-        for it in self.session.query(Period).filter(
+        return sum(
+            (min(it.end, p.end) - max(it.begin, p.begin)).total_seconds()
+            for it in self.session.query(Period).filter(
                     # str по тому что в sqlite datetime хранится как строка
                     (Period.end > str(p.begin)) & (Period.begin < str(p.end))
-                ):
-            total_time += (min(it.end, p.end) - max(it.begin, p.begin)).total_seconds()
-        return total_time
-
-    def add_period(self, period: Period):
-        """
-        добавляет указаный промежуток времени в базу
-        """
-
-        pass  # TODO
+                )
+        )
